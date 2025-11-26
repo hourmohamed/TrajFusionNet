364ccfb9cabff0f14439ca1cccda59d129abe9f5
Jana Gh <janamohamedgh7@gmail.com>
2025-11-26 21:20:29 +0200

    debugging 12 ben2arrab

--- utils/action_predict_utils/run_in_subprocess.py
+++ utils/action_predict_utils/run_in_subprocess.py
@@ -1,13 +1,16 @@
 import subprocess
 
 def run_and_capture_model_path(command: list):
+    print("run_and_capture_model_path")
     captured_output, model_path = [], ""
     with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
         for line in process.stdout:
+            print("Loop")
             print(line, end="")
             captured_output.append(line)
     prefix = "Model saved under: "
     for l in reversed(captured_output):
+        print("loop 2")
         if prefix in l:
             model_path = l[len(prefix):].strip()
     return model_path
